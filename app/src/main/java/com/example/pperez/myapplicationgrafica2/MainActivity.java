1dcd56c3aaad0b2053816f2d90d3fc0c6941e39d
amuii <pperez180372@gmail.com>
2015-02-26 16:27:10 +0100

    otra2

--- app/src/main/java/com/example/pperez/myapplicationgrafica2/MainActivity.java
+++ app/src/main/java/com/example/pperez/myapplicationgrafica2/MainActivity.java
@@ -1,28 +1,19 @@
 package com.example.pperez.myapplicationgrafica2;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.BitmapDrawable;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    signalView gr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(new MyView(this));//R.layout.activity_main);
         setContentView(R.layout.activity_main);
-
+/*
         Paint paint = new Paint();
         paint.setColor(Color.parseColor("#CD5C5C"));
         Bitmap bg = Bitmap.createBitmap(480, 800, Bitmap.Config.ARGB_8888);
@@ -30,32 +21,10 @@ public class MainActivity extends ActionBarActivity {
         canvas.drawRect(50, 50, 200, 200, paint);
         FrameLayout ll = (FrameLayout) findViewById(R.id.Grafica);
         ll.setBackgroundDrawable(new BitmapDrawable(bg));
+*/
+        gr=new signalView(this);
 
     }
-    public class MyView extends View {
-        public MyView(Context context) {
-            super(context);
-            // TODO Auto-generated constructor stub
-        }
-
-        @Override
-        protected void onDraw(Canvas canvas) {
-            // TODO Auto-generated method stub
-            super.onDraw(canvas);
-            int x = getWidth();
-            int y = getHeight();
-            int radius;
-            radius = 100;
-            Paint paint = new Paint();
-            paint.setStyle(Paint.Style.FILL);
-            paint.setColor(Color.WHITE);
-            canvas.drawPaint(paint);
-            // Use Color.parseColor to define HTML colors
-            paint.setColor(Color.parseColor("#CD5C5C"));
-            canvas.drawCircle(x / 2, y / 2, radius, paint);
-        }
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
